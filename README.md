6c764beea640cbc1e0c12955b14358906f2aafb8
Jamis Charles <jacharles@paypal.com>
2015-01-24 11:41:19 -0800

    Initial commit
    Working so far:
    - Pull Open PRs from a hardcoded repo.
    
    Next:
    - Auto pull in the repo from the current folder.
    - Able to pass in the repo to look at via name/repo

---
+++ README.md
@@ -0,0 +1,8 @@
+# Gitt
+Git sugar for common commands. Teaches you best practices, and common use cases.
+
+### TODO:
+
+- Auto-select the repo that you are currently in (look at how hub does it)
+- able to pass in repo from name/repo
+- figure out line wrapping
---
+++ index.js
@@ -0,0 +1,71 @@
+/**
+ * Features:
+ * - Diff
+ * - Show my open PRs (against the current project)
+ * - stats
+ * - git log and search like stephen mentioned
+ * - code search
+ * - comment search
+ * - compare local to remote
+ * - author search
+ * - find lost things
+ * - find when change was introduced
+ */
+
+/**
+ * TODO:
+ * - consider using this https://github.com/mikedeboer/node-github
+ * - study this if I have problems https://www.npmjs.com/package/pull-report
+ * - https://github.com/ForbesLindesay/github-basic#options
+ *
+ * MVP1: List open PRs in public
+ * MVP2: List open PRs for Github enterprise
+ * - allow line wrapping
+ * - allow open PR # from cli
+ */
+
+
+/**
+ * Goals:
+ * - This is partially sugar, and partially to teach git best practictes and more
+ * about Git. It will teach you what a command is actually doing. Make it easier
+ * for common use cases that are really helpful but people don't know how to do.
+ *
+ * This could even be a code-school thing at some point...
+ *
+ * Since it's a side thing it should be small and stupid, but it can also be fun...
+ */
+
+/**
+* Module dependencies.
+*/
+var program = require('commander');
+
+/**
+* Local libs
+*/
+var gitt = require('./lib/actions.js');
+
+var pkg = require('./package.json');
+var version = pkg.version;
+
+// Version and Options
+program
+.version(version)
+// .option('-c, --country [country]', 'some option ')
+
+// Commands
+program
+.command('pr')
+.description('Print open PRs on this repo')
+.action(function (stage) {
+  // program.country gets pulled from the options passed in and set using .option above
+  var config = {
+    // 'country': program.country
+  };
+  gitt.getOpenPRs(stage, config);
+});
+
+
+//this kicks everything off and is required
+program.parse(process.argv);
